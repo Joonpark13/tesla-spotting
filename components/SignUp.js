f703a0c08c33c1373ed6795361a37445aff30037
Joon Park <joon.park13@gmail.com>
2017-08-17 18:17:52 -0500

    Style signup page

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image, View, Text, TextInput, Button } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Platform,
+} from 'react-native';
 
 import tesla from '../images/tesla_t_grey.png'
 
@@ -16,18 +24,26 @@ export default class SignUp extends Component {
         <Image source={tesla} style={styles.logo} />
 
         <View style={styles.loginOptionsContainer}>
-          <Text>Sign up</Text>
+          <View style={styles.suboptionContainer}>
+            <Text style={styles.sectionTitle}>Sign up</Text>
 
-          <Text>Email</Text>
-          <TextInput />
-          <Text>Password</Text>
-          <TextInput />
-          <Text>Confirm Password</Text>
-          <TextInput />
-          <Button title="Create Account" />
+            <View style={styles.inputContainer}>
+              <TextInput style={Platform.OS === 'ios' ? styles.iOSInput : undefined} placeholder="Email" />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput style={Platform.OS === 'ios' ? styles.iOSInput : undefined} placeholder="Password" />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput style={Platform.OS === 'ios' ? styles.iOSInput : undefined} placeholder="Confirm Password" />
+            </View>
+            <Button title="Create Account" />
+          </View>
 
-          <Text>Already have an account?</Text>
-          <Button title="Login" onPress={() => navigate('Login')} />
+          <View style={styles.suboptionContainer}>
+            <Text style={styles.sectionTitle}>Already have an account?</Text>
+
+            <Button title="Login" onPress={() => navigate('Login')} />
+          </View>
         </View>
       </View>
     );
@@ -38,14 +54,33 @@ const styles = StyleSheet.create({
   logo: {
     resizeMode: 'contain',
     maxWidth: 100,
-    maxHeight: 150,
+    maxHeight: 110,
   },
   loginPageContainer: {
     display: 'flex',
     alignItems: 'center',
     backgroundColor: 'white',
+    paddingTop: 20,
   },
   loginOptionsContainer: {
     width: '100%',
+    paddingHorizontal: 16,
+  },
+  suboptionContainer: {
+    marginBottom: 36,
+  },
+  sectionTitle: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 8,
+  },
+  inputContainer: {
+    marginBottom: 6,
+  },
+  iOSInput: { // iOS
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1, 
+    marginVertical: 8,
   },
 });
