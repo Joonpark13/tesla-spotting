72770791b95da7bd4f7479ac02da4ccdde13f323
Joon Park <joon.park13@gmail.com>
2017-08-18 17:25:55 -0500

    Add logout button

--- components/Profile.js
+++ components/Profile.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Button } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+
+import firebase from './firebase';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false,
+    };
+
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  handleLogOut() {
+    firebase.auth().signOut().then(() => {
+      this.setState({ loggedIn: false });
+
+      // Reset the navigational stack and replace it with SignUp
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [
+          NavigationActions.navigate({ routeName: 'Home' }),
+        ],
+      });
+      this.props.navigation.dispatch(resetAction);
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>profile page temp content</Text>
+        {this.state.loggedIn &&
+          <Button title="Log Out" onPress={this.handleLogOut} />
+        }
       </View>
     );
   }
