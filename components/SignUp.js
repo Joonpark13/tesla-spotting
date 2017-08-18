8a989a66b798de515a769080db241b1d7ba109c9
Joon Park <joon.park13@gmail.com>
2017-08-17 19:28:56 -0500

    Fix eslint error, add confirm password validation

--- components/SignUp.js
+++ components/SignUp.js
@@ -9,102 +9,7 @@ import {
   Platform,
 } from 'react-native';
 
-import tesla from '../images/tesla_t_grey.png'
-
-export default class SignUp extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      email: '',
-      emailValid: true,
-      password: '',
-      passwordValid: true,
-    };
-
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-  }
-
-  static navigationOptions = {
-    header: null,
-  };
-
-  handleEmailChange(email) {
-    this.setState({ email });
-
-    // If there is already a timeout in process, clear it
-    if (this.timeout) clearTimeout(this.timeout);
-
-    this.timeout = setTimeout(() => {
-      this.timeout = null;
-
-      if (email) {
-        // Handle email validation by regex: http://emailregex.com/
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        this.setState({ emailValid: re.test(email) });
-      } else {
-        // If field is empty, don't display error message
-        this.setState({ emailValid: true });
-      }
-    }, 500); // half second wait after typing to validate
-  }
-
-  handlePasswordChange(pass) {
-    this.setState({ pass });
-
-    // Passwords must be longer than 4 letters
-    this.setState({ passwordValid: pass === '' || pass.length > 4 });
-  }
-
-  render() {
-    const { navigate } = this.props.navigation;
-
-    return (
-      <View style={styles.loginPageContainer}>
-        <Image source={tesla} style={styles.logo} />
-
-        <View style={styles.loginOptionsContainer}>
-          <View style={styles.suboptionContainer}>
-            <Text style={styles.sectionTitle}>Sign up</Text>
-
-            <View style={styles.inputContainer}>
-              {!this.state.emailValid && <Text style={styles.errorText}>Invalid Email</Text>}
-              <TextInput
-                style={Platform.OS === 'ios' ? styles.iOSInput : undefined}
-                placeholder="Email"
-                autoCorrect={false}
-                keyboardType='email-address'
-                onChangeText={this.handleEmailChange}
-                value={this.email}
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              {!this.state.passwordValid && <Text style={styles.errorText}>Passwords must be longer than 4 characters</Text>}
-              <TextInput
-                style={Platform.OS === 'ios' ? styles.iOSInput : undefined}
-                placeholder="Password"
-                autoCorrect={false}
-                onChangeText={this.handlePasswordChange}
-                value={this.password}
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput style={Platform.OS === 'ios' ? styles.iOSInput : undefined} placeholder="Confirm Password" />
-            </View>
-            <Button title="Create Account" />
-          </View>
-
-          <View style={styles.suboptionContainer}>
-            <Text style={styles.sectionTitle}>Already have an account?</Text>
-
-            <Button title="Login" onPress={() => navigate('Login')} />
-          </View>
-        </View>
-      </View>
-    );
-  }
-}
+import tesla from '../images/tesla_t_grey.png';
 
 const styles = StyleSheet.create({
   logo: {
@@ -144,3 +49,126 @@ const styles = StyleSheet.create({
     color: 'red',
   },
 });
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      emailValid: true,
+      password: '',
+      passwordValid: true,
+      confirm: '',
+      confirmValid: true,
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleConfirmChange = this.handleConfirmChange.bind(this);
+  }
+
+  handleEmailChange(email) {
+    this.setState({ email });
+
+    // If there is already a timeout in process, clear it
+    if (this.timeout) clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      this.timeout = null;
+
+      if (email) {
+        // Handle email validation by regex: http://emailregex.com/
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        this.setState({ emailValid: re.test(email) });
+      } else {
+        // If field is empty, don't display error message
+        this.setState({ emailValid: true });
+      }
+    }, 500); // half second wait after typing to validate
+  }
+
+  handlePasswordChange(pass) {
+    this.setState({ pass });
+
+    // Passwords must be longer than 4 letters
+    this.setState({ passwordValid: pass === '' || pass.length > 4 });
+  }
+
+  handleConfirmChange(pass) {
+    this.setState({ confirm: pass });
+
+    // Passwords must match
+    this.setState({ confirmValid: pass === this.state.pass });
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <View style={styles.loginPageContainer}>
+        <Image source={tesla} style={styles.logo} />
+
+        <View style={styles.loginOptionsContainer}>
+          <View style={styles.suboptionContainer}>
+            <Text style={styles.sectionTitle}>Sign up</Text>
+
+            <View style={styles.inputContainer}>
+              {!this.state.emailValid && <Text style={styles.errorText}>Invalid Email</Text>}
+              <TextInput
+                style={Platform.OS === 'ios' ? styles.iOSInput : undefined}
+                placeholder="Email"
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                onChangeText={this.handleEmailChange}
+                value={this.email}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              {!this.state.passwordValid &&
+                <Text style={styles.errorText}>Passwords must be longer than 4 characters</Text>
+              }
+              <TextInput
+                style={Platform.OS === 'ios' ? styles.iOSInput : undefined}
+                placeholder="Password"
+                autoCorrect={false}
+                autoCapitalize="none"
+                secureTextEntry
+                onChangeText={this.handlePasswordChange}
+                value={this.password}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              {!this.state.confirmValid &&
+                <Text style={styles.errorText}>Passwords must match</Text>
+              }
+              <TextInput
+                style={Platform.OS === 'ios' ? styles.iOSInput : undefined}
+                placeholder="Confirm Password"
+                autoCorrect={false}
+                autoCapitalize="none"
+                secureTextEntry
+                onChangeText={this.handleConfirmChange}
+                value={this.confirm}
+              />
+            </View>
+            <Button title="Create Account" />
+          </View>
+
+          <View style={styles.suboptionContainer}>
+            <Text style={styles.sectionTitle}>Already have an account?</Text>
+
+            <Button title="Login" onPress={() => navigate('Login')} />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+SignUp.navigationOptions = {
+  header: null,
+};
+
+export default SignUp;
