3c84e8b38b353ceb0dcd66b80a2d87f8d894daa3
Joon Park <joon.park13@gmail.com>
2017-08-19 12:25:07 -0500

    Implement firebase data loading for Teslas component

--- components/Teslas.js
+++ components/Teslas.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import { Platform, View } from 'react-native';
 
+import firebase from './firebase';
 import AndroidToolbar from './AndroidToolbar';
 
 class Teslas extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      teslas: [],
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.teslaListener(user.uid);
+      }
+    });
+  }
+
+  teslaListener(uid) {
+    const ref = firebase.database().ref(`users/${uid}/teslas`);
+    ref.on('value', (snapshot) => {
+      const teslas = [];
+
+      snapshot.forEach((tesla) => {
+        teslas.push({ key: tesla.key, ...tesla.val() });
+      });
+
+      this.setState({ teslas });
+    });
+  }
+
   render() {
     return (
       <View>
