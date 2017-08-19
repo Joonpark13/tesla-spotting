4e28cac3c151a523a0d85fb8114b00c1470b2a45
Joon Park <joon.park13@gmail.com>
2017-08-19 02:58:26 -0500

    Implement firebase connection on spotting page

--- components/Home.js
+++ components/Home.js
@@ -32,6 +32,8 @@ class Home extends Component {
     this.state = {
       loggedIn: false,
     };
+
+    this.handleSpot = this.handleSpot.bind(this);
   }
 
   componentDidMount() {
@@ -44,16 +46,27 @@ class Home extends Component {
     });
   }
 
+  handleSpot(name) {
+    navigator.geolocation.getCurrentPosition((location) => {
+      this.props.navigation.navigate('Spotting', {
+        model: name,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        time: Date.now(),
+      });
+    });
+  }
+
   render() {
     return (
       <View>
         <IOSStatusSpacer />
         {Platform.OS === 'android' && <AndroidToolbar navOpen={() => this.props.navigation.navigate('DrawerOpen')} />}
         <ScrollView style={styles.modelSelect}>
-          <ModelCard image={modelS} name="Model S" handleSpot={() => this.props.navigation.navigate('Spotting', { model: 'Model S' })} />
-          <ModelCard image={modelX} name="Model X" handleSpot={() => this.props.navigation.navigate('Spotting', { model: 'Model X' })} />
-          <ModelCard image={roadster} name="Roadster" handleSpot={() => this.props.navigation.navigate('Spotting', { model: 'Roadster' })} />
-          <ModelCard image={model3} name="Model 3" handleSpot={() => this.props.navigation.navigate('Spotting', { model: 'Model 3' })} />
+          <ModelCard image={modelS} name="Model S" handleSpot={() => this.handleSpot('Model S')} />
+          <ModelCard image={modelX} name="Model X" handleSpot={() => this.handleSpot('Model X')} />
+          <ModelCard image={roadster} name="Roadster" handleSpot={() => this.handleSpot('Roadster')} />
+          <ModelCard image={model3} name="Model 3" handleSpot={() => this.handleSpot('Model 3')} />
           {Platform.OS === 'android' && <View style={styles.spacer} />}
         </ScrollView>
       </View>
--- components/Spotting.js
+++ components/Spotting.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+
+import firebase from './firebase';
 
 class Spotting extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: '',
+      details: '',
+      uid: null,
+    };
+
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        uid: user.uid,
+      });
+    });
+  }
+
+  handleSave() {
+    const navProps = this.props.navigation.state.params;
+
+    const db = firebase.database();
+    const ref = db.ref(`users/${this.state.uid}/teslas`);
+    ref.push().set({
+      model: navProps.model,
+      latitude: navProps.latitude,
+      longitude: navProps.longitude,
+      time: navProps.time,
+      color: this.state.color,
+      details: this.state.details,
+    }).then(() => {
+      const backAction = NavigationActions.back();
+      this.props.navigation.dispatch(backAction);
+    });
+  }
+
   render() {
+    const navProps = this.props.navigation.state.params;
     return (
       <View>
-        <Text>{this.props.navigation.state.params.model}</Text>
+        <Text>{navProps.model}</Text>
+        <Text>Latitude</Text>
+        <Text>{navProps.latitude}</Text>
+        <Text>Longitude</Text>
+        <Text>{navProps.longitude}</Text>
+        <Text>Time</Text>
+        <Text>{navProps.time}</Text>
+
+        <Text>Color</Text>
+        <TextInput
+          onChangeText={text => this.setState({ color: text })}
+          value={this.state.color}
+        />
+        <Text>Details</Text>
+        <TextInput
+          onChangeText={text => this.setState({ details: text })}
+          value={this.state.details}
+          multiline
+          numberOfLines={4}
+        />
+
+        <Button title="Save" onPress={this.handleSave} />
       </View>
     );
   }
