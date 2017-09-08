c1b074a9f680b59ad0284e4f81979fb981bb617d
Joon Park <joon.park13@gmail.com>
2017-09-07 21:51:45 -0400

    Change Drawer highlight color

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { DrawerNavigator, TabNavigator } from 'react-navigation';
+import DrawerItems from 'react-navigation/src/views/Drawer/DrawerNavigatorItems';
 
 import Home from './Home';
 import Profile from './Profile';
 import Teslas from './Teslas';
+import { colors } from './helpers';
 
 const MainNav = Platform.OS === 'ios'
   ? (
@@ -32,6 +34,9 @@ const MainNav = Platform.OS === 'ios'
       },
     }, {
       drawerWidth: 150,
+      contentComponent: props => (
+        <DrawerItems {...props} activeTintColor={colors.teslaRed} />
+      ),
     })
   );
 
